3ae1b58f65f739c03f3c802be3ccaa0376ac7ae3
“Gauarv <gauarvmali846@gmail.com>
2023-07-13 13:10:36 +0530

    SQL first project

---
+++ Library Management/librarymanagment.sql
@@ -0,0 +1,12 @@
+ INSERT INTO Book_Details (book_id,book_name, book_author, book_type,book_price)
+  VALUES
+  (1,'A Brief Histroy of Time','Dr. Stephen Hawking','Scientific',350),
+  (2,'Deep Work','Cal Newport','Self - Help',300),
+  (3,'Wings of Fire','Dr. A.P.J. Abdul Kalam and Arun Tiwari','Autobiography',400),
+  (4,'The Monk Who Sold His Ferrari','Robin Sharma','Self - Help',350),
+  (5,'Three Thousand Stiches','Sudha Murthy','Novel',200),
+  (6,'The Richest Man in Babylon','George Clason','Business and Economics',180),
+  (7,'Rich Dad Poor Dad','Robert Kiyosaki','Economics',240),
+  (8,'Think And Grow Rich','Napoleon Hill','Economics',270),
+  (9,'George Secret Key to the Universe','Lucy Hawking','Science fictional story',320),
+  (10,'The Power of Your Subconscious Mind','Dr. Joseph Murphy','Psychology',420);
